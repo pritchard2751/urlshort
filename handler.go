f5f2c5cd436f5d1f4daf77cbe6acb1ba489b98ce
pritchard2751 <annie@growkudos.com>
2018-12-28 16:42:10 +0000

    Cleaned up the handler.go file

--- handler.go
+++ handler.go
@@ -8,10 +8,16 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+// A struct for unmarshaling the YAML/JSON data
+type PathStore []struct {
+    Path string `json:"path",omitempty yaml:"path,omitempty"`
+	Url  string `json:"url",omitempty yaml:"url,omitempty"`
+}
+
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 	// As the return type is http.Handlerfunc, this function is coerced into 
 	// satisfying the Handler interface
-	fmt.Println("Running Map Handler")
+	fmt.Println("Map Handler")
 	return func(w http.ResponseWriter, r *http.Request){
 		path := r.URL.Path
 		longURL, ok := pathsToUrls[path]
@@ -25,38 +31,20 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 	}
 }
 
-// A struct for unmarshaling the data
-type PathStore []struct {
-    Path string `json:"path",omitempty yaml:"path,omitempty"`
-	Url  string `json:"url",omitempty yaml:"url,omitempty"`
-}
-
-func parseYAML(yml []byte, urlPaths *PathStore) error {
-	fmt.Println("Attempting to parse YAML")
-	err := yaml.Unmarshal(yml, urlPaths)
-	return err
-}
-
-func parseJSON(jsonData []byte, urlPaths *PathStore) error {
-	fmt.Println("Attempting to parse JSON")
-	err := json.Unmarshal(jsonData, urlPaths)
-    return err
-}
-
 func buildMap(pathStruct PathStore) map[string]string {
-	fmt.Println("Attempting to build map")
+	fmt.Println("Building map")
 	pathMap := make(map[string]string)
 	// Traverse the struct and add data to the map
 	for _, pathdata := range pathStruct {
-		//fmt.Printf("Path data:\n%v\n", pathdata.Path)
 		pathMap[pathdata.Path] = pathdata.Url
 	}
 	return pathMap
 }
 
-func JSONHandler(json []byte, fallback http.Handler) (http.HandlerFunc, error) {
+func JSONHandler(jsonData []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	fmt.Println("JSON Handler")
 	var parsedJson PathStore
-	err := parseJSON(json, &parsedJson)
+	err := json.Unmarshal(jsonData, &parsedJson)
 	if err != nil {
 		print(err)
 		return nil, err
@@ -66,13 +54,12 @@ func JSONHandler(json []byte, fallback http.Handler) (http.HandlerFunc, error) {
 }
 
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	fmt.Println("YAML Handler")
 	var parsedYaml PathStore
-    err := parseYAML(yml, &parsedYaml)
+    err := yaml.Unmarshal(yml, &parsedYaml)
 	if err != nil {
-		print(err)
 		return nil, err
 	}
-	// YAML has been successfully parsed so build the map
 	pathMap := buildMap(parsedYaml)
 	return MapHandler(pathMap, fallback), nil
 }
